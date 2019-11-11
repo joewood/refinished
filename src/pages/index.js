ef622a2c407256599b66f06b4a52862a2cde86c4
Joe Wood <joe.wood@live.com>
2019-11-10 19:47:31 -0500

    layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,171 +8,159 @@ import Banner from "../components/banner"
 // import Countdown from "../components/countdown"
 // import StarRatingComponent from "react-star-rating-component"
 
-class IndexPost extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Item = ({ node }) => (
+    <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={node.id}>
+        <div className="details_List">
+            {node.image === null ? (
+                <div className="no-image">No Image</div>
+            ) : (
+                <Img sizes={node.image.fluid} />
+            )}
 
-  render() {
-    const { data } = this.props
-
-    return (
-      <React.Fragment>
-        <div className="row product-main">
-          {data.data.allContentfulProduct.edges.map(items => (
+            <h2 style={{ flex: "0 0 auto", paddingTop: 10, paddingLeft: 10 }}>
+                <Link to={`/${node.slug}`}>{node.name}</Link>
+            </h2>
+            <p style={{ flex: "1 0 auto", paddingLeft: 10, paddingRight: 10 }}>
+                {node.details.childMarkdownRemark.excerpt}
+            </p>
             <div
-              className="Catalogue__item col-sm-12 col-md-6 col-lg-4"
-              key={items.node.id}
+                style={{
+                    flex: "0 0 auto",
+                    display: "flex",
+                    margin: 10,
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
             >
-              <div className="details_List">
-                {items.node.image === null ? (
-                  <div className="no-image">No Image</div>
-                ) : (
-                  <Img sizes={items.node.image.fluid} />
-                )}
-
-                <div className="details_inner">
-                  <h2>
-                    <Link to={`/${items.node.slug}`}>{items.node.name}</Link>
-                  </h2>
-                  {/* <StarRatingComponent
-                    name="rate1"
-                    starCount={5}
-                    value={items.node.rating}
-                  /> */}
-                  <p>{items.node.details.childMarkdownRemark.excerpt}</p>
-                  <div className="row">
-                    <div className="col-sm-4 align-self-center">
-                      <span className="price">${items.node.price}</span>
-                    </div>
-                    <div className="col-sm-8 text-right align-self-center">
-                      <a
-                        href="#"
-                        className="Product snipcart-add-item"
-                        data-item-id={items.node.slug}
-                        data-item-price={items.node.price}
-                        data-item-image={
-                          items.node.image === null
-                            ? ""
-                            : items.node.image.fluid.src
-                        }
-                        data-item-name={items.node.name}
-                        data-item-url={`/`}
-                      >
+                <div style={{ flex: "0 0 auto" }}>
+                    <span className="price">${node.price || 0}</span>
+                </div>
+                <div style={{ flex: "0 0 auto" }}>
+                    <a href="#" className="Product snipcart-add-item">
                         <i className="fas fa-shopping-bag" />
                         Contact About Item
-                      </a>
-                    </div>
-                  </div>
+                    </a>
                 </div>
-              </div>
             </div>
-          ))}
         </div>
-      </React.Fragment>
-    )
-  }
-}
+    </div>
+)
+
+const IndexPost = ({ data }) => (
+    <>
+        <div className="row product-main">
+            {data.data.allContentfulProduct.edges.map(items => (
+                <Item node={items.node} />
+            ))}
+        </div>
+    </>
+)
 
 const IndexPage = data => (
-  <Layout>
-    <SEO title="Home" keywords={[`furniture`, `shop`, `restored`, `classic`]} />
-    <Banner BannerData={data.data.allContentfulHeaderBanner.edges} />
-    {/* <LatestBlogs data={data.data.allContentfulBlogs} /> */}
-    <div className="container">
-      <div className="text-center">
-        <h2 className="with-underline">Latest Items</h2>
-      </div>
-      <IndexPost data={data}></IndexPost>
-    </div>
-    {/* <Countdown data={data.data.contentfulDealCountDown} /> */}
-  </Layout>
+    <Layout>
+        <SEO
+            title="Home"
+            keywords={[`furniture`, `shop`, `restored`, `classic`]}
+        />
+        <Banner BannerData={data.data.allContentfulHeaderBanner.edges} />
+        {/* <LatestBlogs data={data.data.allContentfulBlogs} /> */}
+        <div className="container">
+            <div className="text-center">
+                <h2 className="with-underline">Latest Items</h2>
+            </div>
+            <IndexPost data={data}></IndexPost>
+        </div>
+        {/* <Countdown data={data.data.contentfulDealCountDown} /> */}
+    </Layout>
 )
 
 export default IndexPage
 
 export const query = graphql`
-  query AboutQuery {
-    allContentfulProduct(limit: 6, sort: { fields: createdAt, order: DESC }) {
-      edges {
-        node {
-          id
-          name
-          slug
-          #     rating
-          image {
-            fluid(maxWidth: 1000) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
+    query AboutQuery {
+        allContentfulProduct(
+            limit: 6
+            sort: { fields: createdAt, order: DESC }
+        ) {
+            edges {
+                node {
+                    id
+                    name
+                    slug
+                    #     rating
+                    image {
+                        fluid(maxWidth: 1000) {
+                            base64
+                            aspectRatio
+                            src
+                            srcSet
+                            srcWebp
+                            srcSetWebp
+                            sizes
+                        }
+                    }
+                    price
+                    details {
+                        childMarkdownRemark {
+                            excerpt(pruneLength: 140)
+                        }
+                    }
+                }
             }
-          }
-          price
-          details {
-            childMarkdownRemark {
-              excerpt(pruneLength: 140)
-            }
-          }
         }
-      }
-    }
-    allContentfulHeaderBanner {
-      edges {
-        node {
-          title
-          subHeading
-          image {
-            fluid(maxWidth: 1800) {
-              base64
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
+        allContentfulHeaderBanner {
+            edges {
+                node {
+                    title
+                    subHeading
+                    image {
+                        fluid(maxWidth: 1800) {
+                            base64
+                            aspectRatio
+                            src
+                            srcSet
+                            srcWebp
+                            srcSetWebp
+                            sizes
+                        }
+                    }
+                }
             }
-          }
         }
-      }
+        # contentfulDealCountDown {
+        #   title
+        #   featureImage {
+        #     fluid(maxWidth: 1800) {
+        #       base64
+        #       aspectRatio
+        #       src
+        #       srcSet
+        #       srcWebp
+        #       srcSetWebp
+        #       sizes
+        #     }
+        #   }
+        #   date(formatString: "D MMMM, YYYY")
+        # }
+        # allContentfulBlogs(limit: 3, sort: { fields: createdAt, order: DESC }) {
+        #   edges {
+        #     node {
+        #       id
+        #       title
+        #       slug
+        #       featureImage {
+        #         fluid(maxWidth: 1120) {
+        #           base64
+        #           aspectRatio
+        #           src
+        #           srcSet
+        #           srcWebp
+        #           srcSetWebp
+        #           sizes
+        #         }
+        #       }
+        #     }
+        #   }
+        # }
     }
-    # contentfulDealCountDown {
-    #   title
-    #   featureImage {
-    #     fluid(maxWidth: 1800) {
-    #       base64
-    #       aspectRatio
-    #       src
-    #       srcSet
-    #       srcWebp
-    #       srcSetWebp
-    #       sizes
-    #     }
-    #   }
-    #   date(formatString: "D MMMM, YYYY")
-    # }
-    # allContentfulBlogs(limit: 3, sort: { fields: createdAt, order: DESC }) {
-    #   edges {
-    #     node {
-    #       id
-    #       title
-    #       slug
-    #       featureImage {
-    #         fluid(maxWidth: 1120) {
-    #           base64
-    #           aspectRatio
-    #           src
-    #           srcSet
-    #           srcWebp
-    #           srcSetWebp
-    #           sizes
-    #         }
-    #       }
-    #     }
-    #   }
-    # }
-  }
 `
--- src/pages/store.js
+++ src/pages/store.js
@@ -6,133 +6,146 @@ import SEO from "../components/seo"
 // import StarRatingComponent from "react-star-rating-component"
 
 class IndexPost extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      NoOfPost: 6,
+    constructor(props) {
+        super(props)
+        this.state = {
+            NoOfPost: 6,
+        }
+        this.handleScroll = this.handleScroll.bind(this)
     }
-    this.handleScroll = this.handleScroll.bind(this)
-  }
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll)
-  }
-
-  handleScroll = () => {
-    var lastScrollY = window.pageYOffset + 1100
-
-    if (lastScrollY > window.outerHeight) {
-      var count = this.state.NoOfPost + 3
-      this.setState({
-        NoOfPost: count,
-      })
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll)
     }
-  }
 
-  render() {
-    const { data } = this.props
-    const { NoOfPost } = this.state
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll)
+    }
 
-    return (
-      <React.Fragment>
-        <div className="row product-main" onScroll={this.onScrollEvent}>
-          {data.data.allContentfulProduct.edges
-            .slice(0, NoOfPost)
-            .map(items => (
-              <div
-                className="Catalogue__item col-sm-12 col-md-6 col-lg-4"
-                key={items.node.id}
-              >
-                <div className="details_List">
-                  {items.node.image === null ? (
-                    <div className="no-image">No Image</div>
-                  ) : (
-                    <Img sizes={items.node.image.fixed} />
-                  )}
+    handleScroll = () => {
+        var lastScrollY = window.pageYOffset + 1100
 
-                  <div className="details_inner">
-                    <h2>
-                      <Link to={`/${items.node.slug}`}>{items.node.name}</Link>
-                    </h2>
-                    {/* <StarRatingComponent
-                    name="rate1"
-                    starCount={5}
-                    value={items.node.rating}
-                  /> */}
-                    <p>{items.node.details.childMarkdownRemark.excerpt}</p>
-                    <div className="row">
-                      <div className="col-sm-6 align-self-center">
-                        <span className="price">${items.node.price}</span>
-                      </div>
-                      <div className="col-sm-6 text-right align-self-center">
-                        <a
-                          href="#"
-                          className="Product snipcart-add-item"
-                          data-item-id={items.node.slug}
-                          data-item-price={items.node.price}
-                          data-item-image={
-                            items.node.image === null
-                              ? ""
-                              : items.node.image.fixed.src
-                          }
-                          data-item-name={items.node.name}
-                          data-item-url={`/`}
-                        >
-                          <i className="fas fa-shopping-bag" />
-                          Contact
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+        if (lastScrollY > window.outerHeight) {
+            var count = this.state.NoOfPost + 3
+            this.setState({
+                NoOfPost: count,
+            })
+        }
+    }
+
+    render() {
+        const { data } = this.props
+        const { NoOfPost } = this.state
+
+        return (
+            <React.Fragment>
+                <div className="row product-main" onScroll={this.onScrollEvent}>
+                    {data.data.allContentfulProduct.edges
+                        .slice(0, NoOfPost)
+                        .map(({ node }) => (
+                            <div
+                                className="Catalogue__item col-sm-12 col-md-6 col-lg-4"
+                                key={node.id}
+                            >
+                                <div className="details_List">
+                                    {node.image === null ? (
+                                        <div className="no-image">No Image</div>
+                                    ) : (
+                                        <Img sizes={node.image.fixed} />
+                                    )}
+
+                                    <h2
+                                        style={{
+                                            flex: "0 0 auto",
+                                            paddingTop: 10,
+                                            paddingLeft: 10,
+                                        }}
+                                    >
+                                        <Link to={`/${node.slug}`}>
+                                            {node.name}
+                                        </Link>
+                                    </h2>
+                                    <p
+                                        style={{
+                                            flex: "1 0 auto",
+                                            paddingLeft: 10,
+                                            paddingRight: 10,
+                                        }}
+                                    >
+                                        {
+                                            node.details.childMarkdownRemark
+                                                .excerpt
+                                        }
+                                    </p>
+                                    <div
+                                        style={{
+                                            flex: "0 0 auto",
+                                            display: "flex",
+                                            margin: 10,
+                                            justifyContent: "space-between",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        <div style={{ flex: "0 0 auto" }}>
+                                            <span className="price">
+                                                ${node.price || 0}
+                                            </span>
+                                        </div>
+                                        <div style={{ flex: "0 0 auto" }}>
+                                            <a
+                                                href="#"
+                                                className="Product snipcart-add-item"
+                                            >
+                                                <i className="fas fa-shopping-bag" />
+                                                Contact About Item
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                 </div>
-              </div>
-            ))}
-        </div>
-      </React.Fragment>
-    )
-  }
+            </React.Fragment>
+        )
+    }
 }
 
 const IndexPage = data => (
-  <Layout>
-    <SEO title="Store" keywords={[`gatsby`, `application`, `react`]} />
-    <div className="container store-page">
-      <IndexPost data={data}></IndexPost>
-    </div>
-  </Layout>
+    <Layout>
+        <SEO title="Store" keywords={[`gatsby`, `application`, `react`]} />
+        <div className="container store-page">
+            <IndexPost data={data}></IndexPost>
+        </div>
+    </Layout>
 )
 
 export default IndexPage
 
 export const query = graphql`
-  query StoreQuery {
-    allContentfulProduct {
-      edges {
-        node {
-          id
-          name
-          slug
-          #    rating
-          image {
-            fixed(width: 1000, height: 500) {
-              width
-              height
-              src
-              srcSet
+    query StoreQuery {
+        allContentfulProduct {
+            edges {
+                node {
+                    id
+                    name
+                    slug
+                    #    rating
+                    image {
+                        fixed(width: 1000, height: 500) {
+                            width
+                            height
+                            src
+                            srcSet
+                        }
+                    }
+                    price
+                    details {
+                        childMarkdownRemark {
+                            excerpt(pruneLength: 140)
+                        }
+                    }
+                }
             }
-          }
-          price
-          details {
-            childMarkdownRemark {
-              excerpt(pruneLength: 140)
-            }
-          }
         }
-      }
     }
-  }
 `
