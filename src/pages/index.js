cf57ab9134f928a56be2282d131448360a76bee9
Joe Wood <joe.wood@live.com>
2019-11-11 10:44:11 -0500

    link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,67 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React, { useCallback } from "react"
+import { Link, graphql, history } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Banner from "../components/banner"
-// import LatestBlogs from "../components/latestBlog"
-// import Countdown from "../components/countdown"
-// import StarRatingComponent from "react-star-rating-component"
 
-const Item = ({ node }) => (
-    <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={node.id}>
-        <div className="details_List">
-            {node.image === null ? (
-                <div className="no-image">No Image</div>
-            ) : (
-                <Img sizes={node.image.fluid} />
-            )}
-
-            <h2 style={{ flex: "0 0 auto", paddingTop: 10, paddingLeft: 10 }}>
-                <Link to={`/${node.slug}`}>{node.name}</Link>
-            </h2>
-            <p style={{ flex: "1 0 auto", paddingLeft: 10, paddingRight: 10 }}>
-                {node.details.childMarkdownRemark.excerpt}
-            </p>
-            <div
-                style={{
-                    flex: "0 0 auto",
-                    display: "flex",
-                    margin: 10,
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                }}
-            >
-                <div style={{ flex: "0 0 auto" }}>
-                    <span className="price">${node.price || 0}</span>
-                </div>
-                <div style={{ flex: "0 0 auto" }}>
-                    <a href="#" className="Product snipcart-add-item">
-                        <i className="fas fa-shopping-bag" />
-                        Contact About Item
+const Item = ({ node }) => {
+    const onClick = useCallback(() => history.push(node.slug))
+    return (
+        <div
+            className="Catalogue__item col-sm-12 col-md-6 col-lg-4"
+            key={node.id}
+        >
+            <div className="details_List">
+                {node.image === null ? (
+                    <div className="no-image">No Image</div>
+                ) : (
+                    <a href={"/" + node.slug}>
+                        <Img sizes={node.image.fluid} />
                     </a>
+                )}
+
+                <h2
+                    style={{
+                        flex: "0 0 auto",
+                        paddingTop: 10,
+                        paddingLeft: 10,
+                    }}
+                >
+                    <Link to={`/${node.slug}`}>{node.name}</Link>
+                </h2>
+                <p
+                    style={{
+                        flex: "1 0 auto",
+                        paddingLeft: 10,
+                        paddingRight: 10,
+                    }}
+                >
+                    {node.details.childMarkdownRemark.excerpt}
+                </p>
+                <div
+                    style={{
+                        flex: "0 0 auto",
+                        display: "flex",
+                        margin: 10,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <div style={{ flex: "0 0 auto" }}>
+                        <span className="price">${node.price || 0}</span>
+                    </div>
+                    <div style={{ flex: "0 0 auto" }}>
+                        <a href="#" className="Product snipcart-add-item">
+                            <i className="fas fa-shopping-bag" />
+                            Contact About Item
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 const IndexPost = ({ data }) => (
     <>
