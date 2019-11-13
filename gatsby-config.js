165c40652612ea793dc2e1a88e01c01044d111bf
Joe Wood <joe.wood@live.com>
2019-11-12 21:19:36 -0500

    Config Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,51 @@
 var dotenv = require("dotenv")
 dotenv.config()
 
-const { spaceId, accessToken, snipcart } = process.env
+const { spaceId, accessToken } = process.env
 
 module.exports = {
-  siteMetadata: {
-    title: `Refined Refinished`,
-    description: `Restored Furniture`,
-    author: `@woodjoe`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Refined Refinished`,
+        description: `Restored Furniture`,
+        author: `@woodjoe`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `RefinedRefinished`,
-        short_name: `refinished`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/refined-refinished.png`,
-      },
-    },
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId,
-        accessToken,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-snipcart",
-      options: {
-        apiKey: snipcart,
-        autopop: true,
-      },
-    },
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `RefinedRefinished`,
+                short_name: `refinished`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/refined-refinished.png`,
+            },
+        },
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                spaceId,
+                accessToken,
+            },
+        },
+        // {
+        //   resolve: "gatsby-plugin-snipcart",
+        //   options: {
+        //     apiKey: snipcart,
+        //     autopop: true,
+        //   },
+        // },
+    ],
 }
