0a7cf172afa702b9f9a63d63ba1086cd1fa0d7ff
Joe Wood <mail@joewood.me>
2024-01-14 21:06:49 -0500

    Wagz

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        "custom-green": "#2A7C6F",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./**/*.html"],
+};
